0056f6eab8948c4a19ed56b9095568065b5a4e2b
tarek-lazaar <tarek.lazaar@cloudreach.com>
2021-12-09 20:48:25 +0100

    new commit

--- main.tf
+++ main.tf
@@ -3,56 +3,11 @@ provider "aws" {
 }
 
 terraform {
-  required_version = ">=0.12.13"
+
   backend "s3" {
-    bucket         = aws_s3_bucket.state_bucket.bucket
-    key            = "terraform.tfstate"
-    region         = "eu-west-1"
-    dynamodb_table = aws_dynamodb_table.tf_lock_state.name
-    encrypt        = true
-  }
-}
-
-
-#dynamodb table for lock state
-resource "aws_dynamodb_table" "tf_lock_state" {
-  name = var.DynamoDB_table_name
-
-  billing_mode = "PAY_PER_REQUEST"
-
-  hash_key = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name    = var.DynamoDB_table_name
-    BuiltBy = "Terraform"
-  }
-}
-
-resource "aws_s3_bucket" "state_bucket" {
-  bucket = var.S3_bucket_name
-
-  # Tells AWS to encrypt the S3 bucket at rest by default
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
-    }
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-  versioning {
-    enabled = true
-  }
-
-  tags = {
-    Terraform = "true"
+    bucket = "bucketoftarek8786"
+    key    = "terraform.tfstates"
+    dynamodb_table = "terraform-db-table"
   }
 }
 
